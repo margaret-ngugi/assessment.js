764a55fdf54c74aa2fde1d384c6b45a21bace858
margaret-ngugi <ngugiwairimumargaret@gmail.com>
2023-04-11 08:58:22 +0300

    first commit

---
+++ fuctions.js
@@ -0,0 +1,82 @@
+function food(string){
+    for(i=0;string<string.length;i++)
+    let i = 1
+
+}
+
+//Q2
+function numbers(num,target){
+    if(target<num.length){
+        return num
+    }
+    let middle= Math.floor((left+right)/2);
+    let left = num.slice(0,middle);
+    let right = num.slice(middle);
+    var sortedArray(numbers(left),numbers(right));
+}
+function sortedArray(left,right){
+    while(left.length && right.length){
+        let emptyArray=[]
+        if(num == target){
+            emptyArray.push(sortedArray.shift())
+
+        }
+        else if(num<target){
+            emptyArray.push(sortedArray.shift())
+        }
+    }
+    return null
+}
+console.log(...emptyArray,...left,...right)
+let num =[2,8,0,23,5,45,76]
+target=23
+
+
+//Q3
+function sortedArray(years){
+    for(i=0;years<years.length;i++)
+    let i =1
+    if(years /=4){
+    return "this is a leap year"
+    }else{
+        return "this is not a leap year"
+    }
+
+}
+
+    
+
+
+
+//Q4
+function numArray(numbers){
+    for(i=0;numbers<numbers.length;i++)
+    let i = 1
+    if(numbers /=3){
+        return fizz
+    }
+    else if(numbers /=5 ){
+        return buzz
+    }else(numbers ==5&& numbers==3){
+        return fizzbuzz
+    }
+       
+}
+console.log(numArray[numbers])
+
+
+
+//Q5
+function number(numArray){
+    for(i=0;numArray<numArray.length;i++)
+    let i= 1
+    numArray *= 4
+}
+console.log(numArray)
+let numArray = [12,87,45,75,23,64,73]
+
+//Q6
+function nums(strings){
+    for(i=0;strings<strings.length;i++)
+    console.log(strings[i]);
+}
